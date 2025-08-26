51c08052ca50dc784a5ff442e622b29d6a873720
Simbiat23 <155582634+Simbiat23@users.noreply.github.com>
2025-08-26 17:34:23 +0100

    testing the api

--- stock.py
+++ stock.py
@@ -1,13 +1,34 @@
 from rich import print
-import yfinance as yf
+import requests as requests 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Define the ticker symbol
-ticker_symbol = "AAPL"
+st.title("📊 Stock Market Dashboard")
 
-# Create a Ticker object
-ticker = yf.Ticker(ticker_symbol)
+stock_options = st.selectbox('Choose a stock', ['AAPL', 'MSFT', 'TSLA'])
 
-# Fetch historical market data
-historical_data = ticker.history(period="1y")  # data for the last year
-print("Historical Data:")
-print(historical_data)
+api_key = "WUYIYQJYQDMWNQGH"
+company = stock_options
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_options}&apikey={api_key}"
+response = requests.get(url)
+get_data = response.json()
+df = pd.DataFrame(get_data ['Time Series (Daily)']).T
+df = df.rename(columns={
+    "1. open": "Open",
+    "2. high": "High",
+    "3. low": "Low",
+    "4. close": "Close",
+    "5. volume": "Volume"
+})
+df.index = pd.to_datetime(df.index)
+df = df.astype(float)
+
+st.subheader(f"{company} Closing Price (Last 100 Days)")
+fig, ax = plt.subplots()
+df["Close"].head(100).plot(ax=ax, title=f"{company} Closing Prices")
+st.pyplot(fig)
+
+# Show last 5 rows in a table
+st.subheader("Latest Data")
+st.dataframe(df.head())
